24419766ba67f792e921e6b7351aebe33bf71cea
Путевской Данила <putevskoidanila21@gmail.com>
2026-09-17 18:55:46 +0300

    Lab01: variables and interpolation

---
+++ Documents/Lab01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+String myName = "Данила Путевской";
+String groupName = "ИСП-251";
+int courseNumber = 2;
+double averageGrade = 4.8;
+bool isBudget = true;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");
